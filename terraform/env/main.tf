c5b92f78d4280c45e9b5044fc87e0e4abb988335
Fabiano Silva <fabiano.silva@swonelab.com>
2023-01-15 22:14:43 -0300

    ajuste na subnet de Firewall do hub

--- terraform/env/main.tf
+++ terraform/env/main.tf
@@ -102,8 +102,8 @@ module "vnet" {
       rg_name          = module.resource_group.rg_info.hub.name
       vnet_name        = module.vnet.vnet_info.hub.name
     }
-    hub_InboundSubnet = {
-      name             = "InboundSubnet"
+    hub_AzureFirewallSubnet = {
+      name             = "AzureFirewallSubnet"
       address_prefixes = ["10.1.3.0/24"]
       rg_name          = module.resource_group.rg_info.hub.name
       vnet_name        = module.vnet.vnet_info.hub.name
